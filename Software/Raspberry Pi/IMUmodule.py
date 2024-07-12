2fc32ad100ebcd8eb960ee3627ebce92cdf03106
[JaviLendi] <jlendinez@icloud.com>
2024-07-12 17:28:22 +0200

    Improvements to IMU data readings

--- Software/Raspberry Pi/IMUmodule.py
+++ Software/Raspberry Pi/IMUmodule.py
@@ -12,7 +12,7 @@ import board
 import adafruit_icm20x
 
 # Función para inicializar el sensor ICM
-def initialize_icm_sensor():
+def initialize_sensor():
     i2c = board.I2C()  # Utiliza board.SCL y board.SDA por defecto
     icm = adafruit_icm20x.ICM20948(i2c)
     return icm
@@ -29,49 +29,26 @@ def read_gyro(icm):
 def read_magnetic(icm):
     return icm.magnetic
 
+def read_sensor_data(icm):
+    try:
+        acceleration, gyro, magnetic = read_acceleration(icm), read_gyro(icm), read_magnetic(icm)
+        return {"acceleration": acceleration, "gyro": gyro, "magnetic": magnetic}
+    except Exception as e:
+        print(f"Error reading sensor data: {e}")
+        return None
+    
 # Función principal para ejecución continua
 def main():
     icm = initialize_icm_sensor()
-    
-    try:
-        while True:
-            acceleration = read_acceleration(icm)
-            gyro = read_gyro(icm)
-            magnetic = read_magnetic(icm)
-            
-            print("Acceleration: X:%.2f, Y:%.2f, Z:%.2f m/s^2" % acceleration)
-            print("Gyro X:%.2f, Y:%.2f, Z:%.2f rads/s" % gyro)
-            print("Magnetometer X:%.2f, Y:%.2f, Z:%.2f uT" % magnetic)
-            print("")
-            
-            time.sleep(0.5)
-    
-    except KeyboardInterrupt:
-        print("Program interrupted. Exiting...")
+    while True:
+        sensor_data = read_sensor_data(icm)
+        if sensor_data:
+            print(f"Acceleration: {sensor_data['acceleration']}, Gyro: {sensor_data['gyro']}, Magnetic: {sensor_data['magnetic']}")
+        else:
+            print("Error initializing the sensor")
+        time.sleep(1)
 
 # Ejecutar el sensor si se ejecuta como script principal
 if __name__ == "__main__":
     main()
 
-"""
-from icm20948module import initialize_icm_sensor, read_acceleration, read_gyro, read_magnetic
-import time
-
-def main():
-    icm = initialize_icm_sensor()
-    
-    while True:
-        acceleration = read_acceleration(icm)
-        gyro = read_gyro(icm)
-        magnetic = read_magnetic(icm)
-        
-        print("Acceleration: X:%.2f, Y:%.2f, Z:%.2f m/s^2" % acceleration)
-        print("Gyro X:%.2f, Y:%.2f, Z:%.2f rads/s" % gyro)
-        print("Magnetometer X:%.2f, Y:%.2f, Z:%.2f uT" % magnetic)
-        print("")
-        
-        time.sleep(1)
-
-if __name__ == "__main__":
-    main()
-    """
