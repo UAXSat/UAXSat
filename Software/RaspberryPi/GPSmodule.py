43bb22b4e00fddd71b87e8094066234488a59378
[JaviLendi] <jlendinez@icloud.com>
2024-07-13 19:46:00 +0200

    GPS

--- Software/RaspberryPi/GPSmodule.py
+++ Software/RaspberryPi/GPSmodule.py
@@ -67,10 +67,11 @@ def valid_checksum(nmea_sentence):
         calc_checksum = 0
         for char in sentence:
             calc_checksum ^= ord(char)
-        return f"{calc_checksum:02X}" == checksum.upper()
+        return f"{calc_checksum:01X}" == checksum.upper()
     
     except Exception as e:
         print(f"Error calculating checksum: {e}", nmea_sentence)
+        time.sleep(5)
         return False
 
 def nmea_to_decimal(coord, direction):
