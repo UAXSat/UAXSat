7608f5833e9f5152337262e8aaf7f39cbfa4e23c
[JaviLendi] <jlendinez@icloud.com>
2024-07-13 20:06:09 +0200

    --

--- Communications/Sensors/IMUmodule.py
+++ Communications/Sensors/IMUmodule.py
@@ -47,7 +47,7 @@ def read_sensor_data(icm):
         return {"acceleration": acceleration, "gyro": gyro, "magnetic": magnetic}
     except Exception as e:
         print(f"Error reading sensor data: {e}")
-        return None
+        return {"acceleration": acceleration, "gyro": gyro, "magnetic": magnetic}
     
 # Función principal para ejecución continua
 def main():
