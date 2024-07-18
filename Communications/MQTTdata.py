c14cca553117aaaa7bfebc3edc1bcd64b52d3289
[JaviLendi] <jlendinez@icloud.com>
2024-07-18 18:24:44 +0200

    -

--- Communications/MQTTdata.py
+++ Communications/MQTTdata.py
@@ -263,7 +263,7 @@ if __name__ == "__main__":
         gps_parser = GPSParser(BAUDRATE, TIMEOUT, description=DESCRIPTION, hwid=HWID)
 
         # MQTT Client
-        client = mqtt.Client(protocol=mqtt.MQTTv5)
+        client = mqtt.Client()
         client.on_connect = on_connect
         client.on_publish = on_publish
         client.connect(broker, port, 60)
